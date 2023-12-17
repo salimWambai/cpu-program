6b4835bd6355c8ebfcae84489a5919247a6185f3
salim <salimwambai0@gmail.com>
2023-12-17 12:23:47 +0200

    few changes , and added FCFS

--- main.cpp
+++ main.cpp
@@ -4,16 +4,35 @@
 #include <algorithm>
 #include <deque>
 #include <string>
+#include <iomanip>
 
-//task class
-class Task{
-    public:
+//process class
+class Process{
+public:
     std::string name;
     int priority;
     int burstTime;
     int arrivalTime;
     int waitingTime;
     double completed;
-    Task(const std: string& n, int p, int burst)
+
+    Process(const std: string& n, int p, int burst, int arrival)
     : name(n), priority(p), burstTime(burst), arrivalTime(arrival), waitingTime(0),completed(false) {}
+};
+//scheduler funtion
+class Scheduler{
+    public:
+    virtual void scheduleProcesses(std::vector<Process>& processes) = 0;
+};
+//FCFS
+class FCFS : public Scheduler{
+    public:
+    void scheduleProcesses(const std::vector<Process>& processes){
+        std::cout<<"FCFS Scheduling:\n";
+        for(size_t i = 0; i < processes.size(); ++i){
+            const Process& Process = processes[i];
+            std::cout<<"Process Executing: "<<Process.name<<"\n";
+        }
+        std::cout<<"\n";
+    }
 };
